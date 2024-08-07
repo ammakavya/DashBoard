9a57f7b18cc14903c4f16f655d18cee9c5114506
ammakavya <ammakavya872@gmail.com>
2024-08-07 23:58:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
             color: "white",
             fontWeight: "bold",
             marginBottom: 1,
-            marginLeft: "50px",
+            marginLeft: "60px",
           }}
         >
           Dashboard
