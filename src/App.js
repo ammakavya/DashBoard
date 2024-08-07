579137aa2b848020e0af80cddc73564c864f12f8
ammakavya <ammakavya872@gmail.com>
2024-08-07 23:25:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -16,30 +16,30 @@ function App() {
       <Box sx={{ padding: 1 }}>
         <Typography
           variant="h5"
-          sx={{ color: "white", fontWeight: "bold", marginBottom: 1,marginLeft:'125px' }}
+          sx={{
+            color: "white",
+            fontWeight: "bold",
+            marginBottom: 1,
+            marginLeft: "125px",
+          }}
         >
           Dashboard
         </Typography>
         <Grid container spacing={2}>
-          <Grid item xs={12} sm={4} md={0.8}>
+          <Grid item xs={12} sm={4} md={0.5}>
             <MenuBoard />
           </Grid>
-          <Grid item xs={12} sm={8} md={7} sx={{padding:'5px'}}>
-            
-              <ProductDetails />
-              <ActivityDetails />
-                <Transactions/>
-        
-          
+          <Grid item xs={12} sm={8} md={7} sx={{ padding: "5px" }}>
+            <ProductDetails />
+            <ActivityDetails />
+            <Transactions />
           </Grid>
           <Grid item xs={12} sm={12} md={4}>
             <Profits />
             <GoalsPages />
-           
+
             <ReviewPage />
-           
           </Grid>
-         
         </Grid>
       </Box>
     </div>
--- src/components/ReviewPage.js
+++ src/components/ReviewPage.js
@@ -44,7 +44,7 @@ const ReviewPage = () => {
             <Rating
               name={`rating-${index}`}
               defaultValue={review.rating}
-              sx={{ mb: 1 ,borderColor:'white' }}
+              sx={{ mb: 1 ,backgroundColor:'white' }}
             />
             <Typography variant="subtitle2" sx={{ color: "white" }}>
               {review.review}
